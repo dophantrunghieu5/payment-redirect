2365daba84b5ca6b537b5395941ce781699708c1
Hieu Do <hdo@skedulo.com>
2023-11-12 20:26:26 +0800

    Upate direct url

--- src/api/index.js
+++ src/api/index.js
@@ -3,8 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/payment/index', (req, res) => {
-  res.status(302).end('Redirected');
-  res.redirect('/https://google.com');
+  res.status(302).redirect('https://google.com');
 });
 
 module.exports = router;

42e57703337dd7e81e2cc2ec194e458d04ced018
Hieu Do <hdo@skedulo.com>
2023-11-12 20:28:02 +0800

    Update

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/payment/index', (req, res) => {
-  res.status(302).redirect('https://google.com');
+  res.status(302).redirect('https://famous-jalebi-18a9ef.netlify.app/');
 });
 
 module.exports = router;

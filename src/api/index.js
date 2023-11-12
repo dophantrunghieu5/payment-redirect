e6550fe1d8be29280cbe28cb44c163c8f2968ce6
Hieu Do <hdo@skedulo.com>
2023-11-12 20:47:04 +0800

    Update url

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/payment/index', (req, res) => {
-  res.status(302).redirect('https://glittery-trifle-b24c1a.netlify.app/');
+  res.status(302).redirect('eatizenmkp://order/status/0000000097');
+  // res.status(302).redirect('https://glittery-trifle-b24c1a.netlify.app/');
 });
 
 module.exports = router;
